4a1e1eff7d333ea2da411ecb197b79441ff45d1e
QIXIN HE <qhee0011@student.monash.edu>
2020-11-19 23:11:07 +1100

    RegisterVC validate input implemented; project.pbxproj has changed the DEVELOPMENT_TEAM to install it on zoe device.

--- Fantastey/UI/View Controllers/RegisterVC.swift
+++ Fantastey/UI/View Controllers/RegisterVC.swift
@@ -25,6 +25,10 @@ class RegisterVC: UIViewController,UITextFieldDelegate  {
         pwTextField2.delegate = self
         nameTextField.delegate = self
         
+        //因为我点不动两个password textfield（写着strong password
+//        报错：[Assert] View <(null):0x0> does not conform to UITextInput protocol），所以我先disable了这个auto-filling function
+        self.hideKeyboardWhenTappedAround()
+        
         dbController = (UIApplication.shared.delegate as! AppDelegate).dbController
     }
     
@@ -39,18 +43,20 @@ class RegisterVC: UIViewController,UITextFieldDelegate  {
             
             // Sign Up Error
             if let error = error {
-                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
+//                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
+                self.popAlertWindow("Error", error.localizedDescription, false)
                 return
             }
             
             // Sign up Success
-            let alert = UIAlertController(title: "Success", message: "Congratulations!\nYour new account is ready.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default) { (action) in
-                self.navigationController?.popToRootViewController(animated: true)
-            })
-            self.present(alert, animated: true, completion: nil)
+//            let alert = UIAlertController(title: "Success", message: "Congratulations!\nYour new account is ready.", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default) { (action) in
+//                self.navigationController?.popToRootViewController(animated: true)
+//            })
+//            self.present(alert, animated: true, completion: nil)
+            self.popAlertWindow("Success", "Congratulations!\nYour new account is ready.",true)
             
             // Upload cooking level to Firestore
             let id = result!.user.uid
@@ -64,6 +70,29 @@ class RegisterVC: UIViewController,UITextFieldDelegate  {
     }
     
     private func validateEntries() -> Bool {
+        if emailTextField.text == ""{
+            self.popAlertWindow("Error",  "The email account has not provided.", false)
+            return false
+        }
+        if pwTextField1.text != pwTextField2.text {
+            self.popAlertWindow("Error",  "Two password inputs do not match, please check again", false)
+            return false
+        }
+        if nameTextField.text == ""{
+            self.popAlertWindow("Error",  "Please provide a preffered name.", false)
+            return false
+        }
+        let preferredNameLength = nameTextField.text!.count
+        
+        if (preferredNameLength > 20)
+        {
+            self.popAlertWindow("Error",  "The preferred name is now" + String(preferredNameLength) + " characters,which is too long.", false)
+            return false
+        }
+        if cookingLevelSC.selectedSegmentIndex == -1 {
+            self.popAlertWindow("Error",  "Please select a Cooking Level.", false)
+            return false
+        }
         return true
     }
     /*
@@ -79,6 +108,28 @@ class RegisterVC: UIViewController,UITextFieldDelegate  {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool { textField.resignFirstResponder()
         return true
     }
-     
+    
+    //pop up the alert window
+    private func popAlertWindow(_ title:String, _ message:String, _ returnToRootVC:Bool){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if returnToRootVC == true{
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { (action) in
+            self.navigationController?.popToRootViewController(animated: true)
+        })}else{
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))}
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
+//https://stackoverflow.com/questions/24126678/close-ios-keyboard-by-touching-anywhere-using-swift
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
